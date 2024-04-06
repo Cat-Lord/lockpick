2076c3eb22cefa15190ea465c5b768230519b1f5
Cat-Lord <matej.petras18@gmail.com>
2024-04-06 18:09:23 +0200

    feat: hidden cursor and auto-focused canvas on start

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ window.onload = function () {
     throw new Error('Canvas not found.');
   }
 
+  canvas.focus({
+    preventScroll: true,
+  });
   // TODO: make use of dynamic configuration
   const config = new Config(Difficulty.easy, canvas);
   const lock = new Lock(config, canvas.getContext('2d')!);
